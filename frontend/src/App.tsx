22dd9c839c1849b656b1b76338d547008ae32866
ar787 <armanhovhannisyan1002@gmail.com>
2026-06-09 13:51:58 +0400

    add auth guard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { AuthGuard } from "./components/AuthGuard";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { ProjectsPage } from "./pages/ProjectsPage";
@@ -8,7 +9,9 @@ export default function App() {
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/projects" element={<ProjectsPage />} />
+      <Route element={<AuthGuard />}>
+        <Route path="/projects" element={<ProjectsPage />} />
+      </Route>
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
